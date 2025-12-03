6f214262e3343ea44e8c8c819105b10f6467c9e5
hirwacedric123 <hirwacedr12@gmail.com>
2025-12-03 02:01:11 +0200

    improved the login and signup logos to remove background

--- InzuLink/settings.py
+++ InzuLink/settings.py
@@ -202,6 +202,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = '/auth/login/'  # URL where unauthenticated users are redirected
 LOGIN_REDIRECT_URL = 'dashboard'  # Change to your preferred redirect page
 LOGOUT_REDIRECT_URL = 'login'
 
